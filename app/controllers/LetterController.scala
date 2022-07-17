3ec558b354009ccba1ac889d2b9674a5284faf6d
Roman Davis <fluffyribbit@gmail.com>
2022-07-16 20:17:20 -0400

    Got letter-roots join to work; changed name to Dabber

--- app/controllers/LetterController.scala
+++ app/controllers/LetterController.scala
@@ -10,18 +10,12 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc.Results._
 
-import models.User
+import models.Letter
 import models.DB.session
 
 class LetterController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def all() = Action { implicit request =>
     val username: String = request.session.get("username").getOrElse("")
-
-    if(username == ""){
-        Redirect(routes.UserController.home()).flashing(
-            "message" -> "Login to see users."
-            )
-    }
 
     val column: String = request.getQueryString("column").getOrElse("letter")
     val order: String = request.getQueryString("order").getOrElse("ASC")
@@ -32,7 +26,7 @@ class LetterController @Inject()(cc: ControllerComponents) extends AbstractContr
 
   // READ
   def read(name: String) = Action { implicit request =>
-    val letter: models.Letter = models.Letter.read(name)
+    val letter = models.Letter.read(name)
     
     Ok(views.html.letters.read(letter))
   }
--- app/models/Letter.scala
+++ app/models/Letter.scala
@@ -3,6 +3,7 @@ package models
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.{StringType, StructField, StructType, LongType}
+import org.apache.spark.sql.functions._
 
 import org.apache.spark.sql.functions.{desc, asc}
 import DB.session
@@ -32,7 +33,22 @@ object Letter {
     dataframe.createOrReplaceTempView("letters")
 
     // Hack so that methods return User even when not found.
-    val empty: Letter = new Letter("NULL", "NULL", "NULL", "NULL", "NULL")
+    val empty: Letter = new Letter("NULL", "NULL", "NULL", "NULL", "NULL", -1L)
+
+    val counts = models.Root.dataframe
+                    .groupBy("parent")
+                    .agg(count("*")
+                    .as("root count"))
+        
+    val root_counts = counts.select(
+            counts("root count"), counts("parent").as("child-roots")
+        )
+
+    val joined = dataframe.join(
+            root_counts,
+            dataframe("letter") === root_counts("child-roots"),
+            "left_outer"
+        ).na.fill(0)
 
      def show() = {
         // SELECT DISTINCT is a hack here: somehow, when I add a row to my table,
@@ -43,7 +59,7 @@ object Letter {
     }
 
     def all(column: String = "letter", order: String = "ASC"): Array[Letter] = {
-        val sorted = if(order == "ASC") dataframe.sort(asc(column)) else dataframe.sort(desc(column))
+        val sorted = if(order == "ASC") joined.sort(asc(column)) else joined.sort(desc(column))
         sorted.show()
 
         return sorted.rdd.map(row =>
@@ -56,7 +72,7 @@ object Letter {
         // return session.sql(s"SELECT * FROM users where name = $name")
         // For some reason, using the above spark sql is confusing to spark;
         // good riddance; I'd rather user programatic syntax anyway.
-        val dataread = dataframe.filter(dataframe("letter") === string)
+        val dataread = joined.filter(dataframe("letter") === string)
 
         if(dataread.count() > 0){
             return new Letter(dataread.first())
@@ -72,7 +88,7 @@ object Letter {
 
     def create(string: String, glyph: String, name: String, meaning: String, rationale: String): Letter = {
         all()
-        val letter: Letter = new Letter(string, glyph, name, meaning, rationale)
+        val letter: Letter = new Letter(string, glyph, name, meaning, rationale, 0)
 
         letter.dataframe().show()
         dataframe = dataframe.union(letter.dataframe())
@@ -83,20 +99,22 @@ object Letter {
     }
 }
 
-case class Letter(string: String, glyph: String, name: String, meaning: String, rationale: String){
+case class Letter(string: String, glyph: String, name: String, meaning: String, 
+    rationale: String, root_count: Long = 0){
     def this(row: org.apache.spark.sql.Row) = {
         this(
             row.getAs[String]("letter"),
             row.getAs[String]("glyph"),
             row.getAs[String]("name"),
             row.getAs[String]("meaning"),
-            row.getAs[String]("rationale")
+            row.getAs[String]("rationale"),
+            row.getAs[Long]("root count")
         )
     }
 
     def dataframe(): org.apache.spark.sql.DataFrame = {
         return DB.session.createDataFrame(
-            Seq((string, glyph, name, meaning, rationale)))
-        .toDF("letter", "glyph", "name", "meaning", "rationale")
+            Seq((string, glyph, name, meaning, rationale, root_count)))
+        .toDF("letter", "glyph", "name", "meaning", "rationale", "root count")
     }
 }
